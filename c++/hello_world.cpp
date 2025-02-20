ef6ffb6795722a0f9d0afcd143ba4b62fb3c4a16
phael77 <rapha555lima@gmail.com>
2025-02-20 20:21:50 -0300

    reformulação do repositório

--- c++/hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int number;
-    std::cout << "Enter an integer: ";
-    std::cin >> number;
-    std::cout << "You entered: " << number << std::endl;
-    return 0;
-}
